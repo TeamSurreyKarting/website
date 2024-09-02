bedd377f9ed6ab8fdaf871454c63032595e9d7b7
George Nick Gorzynski <georgegorzynski@me.com>
2024-09-02 20:43:11 +0100

    Bad code

---
+++ app/race-league/page.tsx
@@ -0,0 +1,9 @@
+import RaceLeagueResults from "@/app/components/ui/RaceLeagueResults";
+
+export default function RaceLeague() {
+    return (
+        <div>
+            <RaceLeagueResults />
+        </div>
+    );
+}
