44f7b971db1f78c7ec426460706721cb351d1bca
AranJannson <me@aranjannson.com>
2024-09-02 21:44:07 +0100

    Never forget RLS

--- app/components/ui/RaceLeagueResults.tsx
+++ app/components/ui/RaceLeagueResults.tsx
@@ -5,10 +5,7 @@ export default async function RaceLeagueResults() {
 
     const timestampNow = new Date().toISOString();
     let { data: leagueData, error } = await supabase
-        .from('League')
-        .select('id, name')
-        .gte('end_date', timestampNow)
-        .order('start_date', { ascending: false });
+        .from('League').select('id, name')
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -18,17 +15,15 @@ export default async function RaceLeagueResults() {
         <div className="bg-nile-blue-950/30 p-5 w-full md:w-[70%] mx-auto rounded-lg">
             <h2 className="text-2xl font-extrabold text-nile-blue-100 text-center mb-5">Race Leagues</h2>
             <div id="race-leagues" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {leagueData === null ? (
-                    <div>No leagues found</div>
-                ) : (
-                    leagueData.map((league) => (
+                {
+                    leagueData?.map((league) => (
                         <div key={league.id} className="flex items-center bg-nile-blue-200 p-3 rounded-md shadow-sm mb-2">
                             <a href={`/league/${league.id}`}>
                                 <h3 className="text-lg font-semibold">{league.name}</h3>
                             </a>
                         </div>
                     ))
-                )}
+                }
             </div>
         </div>
     );
