3452925dad3d51630326592a71f8e224d9e8bb31
George Nick Gorzynski <georgegorzynski@me.com>
2024-09-29 22:29:30 +0100

    Add Quick Links to Home PAge

--- app/admin/(overview)/page.tsx
+++ app/admin/(overview)/page.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaPiggyBank } from "react-icons/fa6";
+
 export default function Page() {
     return (
         <>
-            <h1 className="text-2xl">Team Surrey Karting Admin Area</h1>
-            <hr className="my-6" />
-            <div className="p-6 rounded-xl bg-nile-blue-700">
-                <p>Quick Actions will go here</p>
+            <h1 className="text-2xl mb-4">Team Surrey Karting Admin Area</h1>
+            <h2 className="text-lg">Quick Links</h2>
+            <div className="grid gap-4 grid-cols-2 lg:grid-cols-4 my-2">
+                <Link href="https://bukc.alphatiming.co.uk/profile" className="flex flex-col gap-4 items-center justify-between bg-nile-blue-700 text-white hover:bg-nile-blue-500 px-4 pt-6 pb-4 rounded-lg">
+                    <Image src="/BUKC/bukc-white.svg" alt="BUKC Logo" width={150} height={100} />
+                    BUKC Admin
+                </Link>
+                <Link href="https://results.alphatiming.co.uk/bukc" className="flex flex-col gap-4 items-center justify-between bg-nile-blue-700 text-white hover:bg-nile-blue-500 px-4 pt-6 pb-4 rounded-lg">
+                    <Image src="/AlphaTiming/logo_with_name.svg" alt="Alpha Timing Logo" width={150} height={100} />
+                    BUKC Results
+                </Link>
+                <Link href="https://my.ussu.co.uk/activity/Pages/ComPanel.aspx?RootFolder=%2Factivity%2FClubDocuments%2Fnone" className="flex flex-col gap-4 items-center justify-between bg-nile-blue-700 text-white hover:bg-nile-blue-500 px-4 pt-6 pb-4 rounded-lg">
+                    <Image src="/StudentUnion/SSU_Logo_H_Left_White.svg" alt="Alpha Timing Logo" width={150} height={100} />
+                    Signatories Panel
+                </Link>
+                <Link href="https://my.ussu.co.uk/activity/Withdrawals/Forms/AllItems.aspx" className="flex flex-col gap-4 items-center justify-between bg-nile-blue-700 text-white hover:bg-nile-blue-500 px-4 pt-6 pb-4 rounded-lg">
+                    <FaPiggyBank size={50} />
+                    Finance Requests
+                </Link>
             </div>
         </>
     );
