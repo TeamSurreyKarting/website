c100bccb69414fb7d4c554099787f6bc5b0aa983
George Nick Gorzynski <georgegorzynski@me.com>
2024-09-29 22:38:57 +0100

    Use `Link` as parent element instead of `div`

--- app/components/admin/racers/list.tsx
+++ app/components/admin/racers/list.tsx
@@ -7,14 +7,12 @@ export default async function RacerList() {
 
     return (
         <>
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
                 {
                 racers?.map((racer) => (
-                    <div key={racer.id} className="bg-nile-blue-700 hover:bg-lightning-gold-300 hover:text-black p-4 rounded-xl">
-                        <Link href={`/admin/racers/${racer.id}`}>
-                            <p className="text-lg">{racer.first_name} {racer.last_name}</p>
-                        </Link>
-                    </div>
+                    <Link href={`/admin/racers/${racer.id}`} key={racer.id} className="bg-nile-blue-700 hover:bg-lightning-gold-300 hover:text-black p-4 rounded-xl">
+                        <p className="text-lg">{racer.first_name} {racer.last_name}</p>
+                    </Link>
                 ))}
             </div>
         </>
