710c09f8ae2da0d55e1356d55b21ed994103e7ad
George Nick Gorzynski <georgegorzynski@me.com>
2024-09-30 01:16:13 +0100

    fix redirect logic

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -1,38 +1,6 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
-export const createClient = (request: NextRequest) => {
-    // Create an unmodified response
-    let supabaseResponse = NextResponse.next({
-        request: {
-            headers: request.headers,
-        },
-    });
-
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll()
-                },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    })
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        supabaseResponse.cookies.set(name, value, options)
-                    )
-                },
-            },
-        },
-    );
-
-    return supabaseResponse
-};
-
 export async function updateSession(request: NextRequest) {
     let supabaseResponse = NextResponse.next({
         request,
@@ -65,16 +33,24 @@ export async function updateSession(request: NextRequest) {
 
     const {
         data: { user },
-    } = await supabase.auth.getUser()
+      } = await supabase.auth.getUser()
 
-    if (
-        !user &&
-        !request.nextUrl.pathname.startsWith('/login') &&
-        !request.nextUrl.pathname.startsWith('/auth')
-    ) {
+    console.log('.startsWith(admin)', request.nextUrl.pathname.startsWith('/admin'))
+    console.log('!user', !user)
+    console.log('if', !user && (
+        request.nextUrl.pathname === '/auth' ||
+        request.nextUrl.pathname.startsWith('/admin')
+    ))
+    
+      if (
+        !user && (
+            request.nextUrl.pathname === '/auth' ||
+            request.nextUrl.pathname.startsWith('/admin')
+        )
+      ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone()
-        url.pathname = '/login'
+        url.pathname = '/auth/login'
         return NextResponse.redirect(url)
     }
 
