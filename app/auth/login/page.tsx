e4fa6db66c2fd6a5b34c65b9ed3ce9dc86b3178a
George Nick Gorzynski <georgegorzynski@me.com>
2024-10-05 20:53:06 +0100

    fix server/client render mismatch

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,15 +2,20 @@
 
 import { createClient } from '@/app/utils/supabase/client';
 import Image from 'next/image'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { AiOutlineLoading } from "react-icons/ai";
 
 export default function LoginPage() {
     const [error, setError] = useState<string | null>(null);
     const [isLoading, setLoading] = useState<boolean>(false);
+    const [randInt, setRandInt] = useState<number>(1);
 
-    const randInt = Math.floor(Math.random() * 10) + 1;
+    const router = useRouter();
+
+    useEffect(() => {
+        setRandInt(Math.floor(Math.random() * 10) + 1);
+    }, []);
 
     const supabase = createClient();
 
@@ -28,16 +33,16 @@ export default function LoginPage() {
             return;
         }
 
-        useRouter().push('/admin');
+        router.push('/admin');
     }
 
     return (
         <div
-            className={`w-full h-screen mx-auto p-8 bg-lightning-gold-500/20 bg-[url('/images/BUKC/${randInt}.jpg')] bg-no-repeat bg-cover flex justify-center items-center`}
+            className={`w-full h-screen mx-auto p-8 bg-nile-blue-950 bg-no-repeat bg-cover flex justify-center items-center`}
             >
             <div className="bg-nile-blue-950 p-4 rounded-xl shadow-lg max-w-[400px]">
                 <div className="flex flex-col items-center">
-                <Image src="/TeamSurreyKarting/Team Surrey Karting Club Logo - Solid Background.png" width={200} height={200} alt='Team Surrey Karting Club Logo' />
+                    <Image src="/TeamSurreyKarting/Team Surrey Karting Club Logo - Solid Background.png" width={200} height={200} alt='Team Surrey Karting Club Logo' />
                     <h1 className="text-2xl font-bold">Sign In</h1>
                 </div>
                 <div>
@@ -54,7 +59,7 @@ export default function LoginPage() {
                             <label htmlFor="email">Email:</label>
                             <input id="email" name="email" type="email" required className="w-full px-3 py-2 bg-nile-blue-700 border border-gray-300 rounded-md shadow-sm focus:ring-lightning-gold-500 focus:border-lightning-gold-500 sm:text-sm" />
                         </div>
-                        <div className="mb-4 w-full">
+                        <div className="mb-4 w-">
                             <label htmlFor="password">Password:</label>
                             <input id="password" name="password" type="password" required className="w-full px-3 py-2 bg-nile-blue-700 border border-gray-300 rounded-md shadow-sm focus:ring-lightning-gold-500 focus:border-lightning-gold-500 sm:text-sm" />
                         </div>
