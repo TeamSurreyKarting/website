3af22dfa27863e145aabb3553dfda9e0cb37a038
George Nick Gorzynski <georgegorzynski@me.com>
2024-10-05 19:17:46 +0100

    Submit login from client

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,36 @@
-import { login } from './actions'
+'use client';
+
+import { createClient } from '@/app/utils/supabase/client';
 import Image from 'next/image'
+import { useState } from 'react';
+import { redirect } from 'next/navigation';
+import { AiOutlineLoading } from "react-icons/ai";
 
 export default function LoginPage() {
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setLoading] = useState<boolean>(false);
+
     const randInt = Math.floor(Math.random() * 10) + 1;
+
+    const supabase = createClient();
+
+    const handleLogin = async (formData: FormData) => {
+        const data = {
+            email: formData.get('email') as string,
+            password: formData.get('password') as string,
+        }
+
+        const { error } = await supabase.auth.signInWithPassword(data);
+
+        if (error) {
+            setLoading(false);
+            setError(error.message);
+            return;
+        }
+
+        redirect('/admin');
+    }
+
     return (
         <div
             className={`w-full h-screen mx-auto p-8 bg-lightning-gold-500/20 bg-[url('/images/BUKC/${randInt}.jpg')] bg-no-repeat bg-cover flex justify-center items-center`}
@@ -10,10 +38,18 @@ export default function LoginPage() {
             <div className="bg-nile-blue-950 p-4 rounded-xl shadow-lg max-w-[400px]">
                 <div className="flex flex-col items-center">
                 <Image src="/TeamSurreyKarting/Team Surrey Karting Club Logo - Solid Background.png" width={200} height={200} alt='Team Surrey Karting Club Logo' />
-                    <h1 className="text-2xl font-bold ">Sign In</h1>
+                    <h1 className="text-2xl font-bold">Sign In</h1>
                 </div>
                 <div>
-                    <form className="flex flex-col items-center justify-stretch" action={login}>
+                    <form 
+                        className="flex flex-col items-center justify-stretch"
+                        onSubmit={(e) => {
+                            setLoading(true);
+                            e.preventDefault();
+                            const formData = new FormData(e.currentTarget);
+                            handleLogin(formData);
+                        }}
+                        >
                         <div className="mb-4 w-full">
                             <label htmlFor="email">Email:</label>
                             <input id="email" name="email" type="email" required className="w-full px-3 py-2 bg-nile-blue-700 border border-gray-300 rounded-md shadow-sm focus:ring-lightning-gold-500 focus:border-lightning-gold-500 sm:text-sm" />
@@ -22,7 +58,9 @@ export default function LoginPage() {
                             <label htmlFor="password">Password:</label>
                             <input id="password" name="password" type="password" required className="w-full px-3 py-2 bg-nile-blue-700 border border-gray-300 rounded-md shadow-sm focus:ring-lightning-gold-500 focus:border-lightning-gold-500 sm:text-sm" />
                         </div>
-                        <button className="bg-lightning-gold-600 w-fit">Log in</button>
+                        {isLoading && <p className="mb-4 text-lightning-gold-600"><AiOutlineLoading className="animate-spin" /></p>}
+                        {(!isLoading && error) && <p className="mb-4 text-red-500">{error}</p>}
+                        <button className="bg-lightning-gold-600 w-full px-4 py-2 rounded-lg">Log in</button>
                     </form>
                 </div>
             </div>
