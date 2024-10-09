9214713e6a482fc1766476209dfa153c5894bf91
George Nick Gorzynski <georgegorzynski@me.com>
2024-10-09 18:17:06 +0100

    fix

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -33,6 +33,6 @@ export function createAnonClient() {
 export function createServiceClient() {
     return createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+        process.env.SUPABASE_SERVICE_ROLE_KEY!
     );
 }
